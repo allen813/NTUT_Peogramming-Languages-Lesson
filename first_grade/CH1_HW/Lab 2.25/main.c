13f8588ced8c60f4bcd547fb2234b225b72ddbb8
allen813 <59205679+allen813@users.noreply.github.com>
2022-10-01 18:19:16 +0800

    Add files via upload

---
+++ first_grade/CH1_HW/Lab 2.25/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("My english name\n is  Liao Hung Yang\n\n");
+	printf("LLLLLLLLL\n");
+	printf("L        \n");
+	printf("L        \n");
+	printf("L        \n");
+	printf("L        \n\n");
+	printf("HHHHHHHHH\n");
+	printf("    H    \n");
+	printf("    H    \n");
+	printf("    H    \n");
+	printf("HHHHHHHHH\n\n");
+	printf("       Y \n");
+	printf("     Y   \n");
+	printf("YYYYY    \n");
+	printf("     Y   \n");
+	printf("       Y \n");
+	system("pause");
+	return 0;
+}
