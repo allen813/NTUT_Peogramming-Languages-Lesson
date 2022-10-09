578de6882c0d05ec543bbf428b1e24af652d0524
allen813 <59205679+allen813@users.noreply.github.com>
2022-10-09 23:01:33 +0800

    Add files via upload

---
+++ first_grade/CH2 HW/Lab 4.31/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int x=4, y=1;
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 0; j < x; j++)	printf(" ");
+		for (int j = 0; j < y; j++)	printf("*");
+		if (i < 5) {
+			x -= 1;
+			y += 2;
+		}
+		else {
+			x += 1;
+			y -= 2;
+		}
+		printf("%s", "\n");
+	}
+}
