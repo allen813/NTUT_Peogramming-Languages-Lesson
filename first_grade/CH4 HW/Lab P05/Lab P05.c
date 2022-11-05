742ac55c1851eb92d8fcbf9616c122ec6dafe76d
allen813 <59205679+allen813@users.noreply.github.com>
2022-11-05 15:15:49 +0800

    Add files via upload

---
+++ first_grade/CH4 HW/Lab P05/Lab P05.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+	int n[10] = { 32,27,64,18,95,14,90,70,60,37 };
+
+	printf("%s%13s\n", "Element", "Value");
+
+	for (int i = 0; i < 10; i++)
+		printf("%7d%13d\n", i, n[i]);
+
+	system("pause");
+	return 0;
+}
